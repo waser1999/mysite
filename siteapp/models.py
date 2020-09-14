65926f7557ae9bece6d3693686f91fcd17c9cd3d
waser1999 <2780925747@qq.com>
2020-09-14 15:16:53 +0800

    0.2
    2/7 已配置数据源

--- siteapp/models.py
+++ siteapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model):
+    plant = models.CharField(max_length = 50, primary_key = True)
+    temp_u = models.IntegerField()
+    temp_b = models.IntegerField()
+    humi_u = models.IntegerField()
+    humi_b = models.IntegerField()
+    co2_u = models.IntegerField()
+    co2_b = models.IntegerField()
+    ischecked = models.IntegerField(max_length = 1)
