1f49faa0e92c38274ca87d0a20205d39f50470ca
waser1999 <2780925747@qq.com>
2020-10-01 14:08:10 +0800

    0.4.3
    界面完全完成！

--- siteapp/views.py
+++ siteapp/views.py
@@ -4,35 +4,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import info
+from django.forms.models import model_to_dict
 
 def index(request):
     return render(request,"select.html")
 
 # 选中一个数据用于工作
 def choose(request):
-    request.encoding = 'utf-8'
-
-    # if request.method == 'POST':
-    #     plantName = request.POST.get('plant','')
-
-    # 修改选中标记
-    # info.objects.all().update(ischecked = 0)
-    # info.objects.filter(plant = plantName).update(ischecked = 1)
+    rvalue = info.objects.all()
+    if request.method == 'POST':
+        plantName = request.POST.get('plant','')
+        # 修改选中标记
+        info.objects.all().update(ischecked = 0)
+        info.objects.filter(plant = plantName).update(ischecked = 1)
     
-    # # 传输相关参数
-    # refer = info.objects.get(plant = plantName)
-    # response = {
-    #     'plant' : plantName,
-    #     'temp_u' : refer.temp_u,
-    #     'temp_b' : refer.temp_b,
-    #     'humi_u' : refer.humi_u,
-    #     'humi_b' : refer.humi_b,
-    #     'co2_u' : refer.co2_u,
-    #     'co2_b' : refer.co2_b,
-    #     'ischecked' : refer.ischecked,
-    # }
-    return render(request,"select.html")
-    # return render(request,"select.html",response)
+    response = {
+        'rvalue': rvalue,
+    }
+    return render(request,"select.html",response)
 
 # 添加一个数据
 def add(request):
@@ -54,29 +43,43 @@ def add(request):
 
 # 删除
 def delete(request):
+    rvalue = info.objects.all()
+    reply = ''
     try:
         if request.method == 'POST':
             plantName = request.POST.get('plant','')
             info.objects.filter(plant = plantName).delete()
-            response = 'Delete successfully'
+            reply = 'Delete successfully'
     except:
-        response = 'Can not find object.'
-    return render(request,"delete.html")
-    # return render(request,"delete.html",response)
+        reply = 'Can not find object.'
+    response = {
+        'rvalue': rvalue,
+        'reply': reply,
+    }
+    return render(request,"delete.html",response)
 
 # 修改
 def modify(request):
-    if request.method == 'POST':
-        plantName = request.POST.get('plant','')
-        column = request.POST.get('column','')          #列名
-        num = request.POST.get('num','')
+    rvalue = info.objects.all()
+    reply = ''
+    try:
+        if request.method == 'POST':
+            plantName = request.POST.get('plant','')
+            column = request.POST.get('column','')          #列名
+            num = request.POST.get('num','')
 
-        info.objects.filter(plant = plantName).update(column = num)
-        response = 'Update successfully'
-    return render(request,"update.html")
-    # return render(request,"update.html",response)
+            info.objects.filter(plant = plantName).update(column = num)
+            reply = 'Update successfully'
+    except:
+        reply = 'Can not find object.'
+    response = {
+        'rvalue': rvalue,
+        'reply': reply,
+    }
+    return render(request,"update.html",response)
 
 # 全部列出        
 def clist(request):
-    # info.objects.all().order_by('ischecked')
-    return render(request,"list.html")
+    rvalue = info.objects.all()
+    response = {'rvalue': rvalue}
+    return render(request,"list.html", response)
